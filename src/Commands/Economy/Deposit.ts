88c4a770fa5394f8ad20a63d76f35e15c2a657d4
Kingshisui00 <127178249+Kingshisui00@users.noreply.github.com>
2023-04-24 20:20:18 +0530

    Create Deposit.ts

---
+++ src/Commands/Economy/Deposit.ts
@@ -0,0 +1,39 @@
+import { BaseCommand, Command, Message } from '../../Structures'
+
+@Command('deposit', {
+    description: 'Deposit your gold to bank',
+    usage: 'deposit',
+    cooldown: 15,
+    exp: 5,
+    category: 'economy'
+})
+export default class command extends BaseCommand {
+    override execute = async (M: Message): Promise<void> => {
+        if (M.numbers.length < 1) return void M.reply('Specify the amount of gold to deposit')
+        const { wallet } = await this.client.DB.getUser(M.sender.jid)
+        if ((wallet - M.numbers[0]) < 0) return void M.reply(`Check your wallet You don't have such a amount of gold`)
+        await this.client.DB.setGold(M.sender.jid, M.numbers[0], 'bank')
+        await this.client.DB.setGold(M.sender.jid, -M.numbers[0])
+        const buttons = [
+            {
+                buttonId: 'id1',
+                buttonText: { displayText: `${this.client.config.prefix}wallet` },
+                type: 1
+            },
+            {
+                buttonId: 'id2',
+                buttonText: { displayText: `${this.client.config.prefix}bank` },
+                type: 1
+            }
+        ]
+        const buttonMessage = {
+            text: `*🎉 You have transferred ${M.numbers[0]}*gold to ur Bank*`,
+            footer: 'Levi 2023 🚀✨',
+            buttons: buttons,
+            headerType: 1
+        }
+        return void (await this.client.sendMessage(M.from, buttonMessage, {
+            quoted: M.message
+        }))
+    }
+}
