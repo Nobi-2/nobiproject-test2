bbe89ee06e588563434fe5a0385fcd6940c15358
Kingshisui00 <127178249+Kingshisui00@users.noreply.github.com>
2023-04-24 20:21:04 +0530

    Create Bank.ts

---
+++ src/Commands/Economy/Bank.ts
@@ -0,0 +1,30 @@
+import { BaseCommand, Command, Message } from '../../Structures'
+
+@Command('bank', {
+    description: '',
+    usage: 'bank',
+    category: 'economy',
+    exp: 10,
+    cooldown: 10
+})
+export default class command extends BaseCommand {
+    override execute = async ({ from, sender, message }: Message): Promise<void> => {
+        const { bank, tag } = await this.client.DB.getUser(sender.jid)
+        const buttons = [
+            {
+                buttonId: 'id1',
+                buttonText: { displayText: `${this.client.config.prefix}wallet` },
+                type: 1
+            }
+        ]
+        const buttonMessage = {
+            text: `🏦 *Bank* 🏦\n\n⛩️ *Name: ${sender.username}*\n\n   💮 *tag: #${tag}*\n\n🪙 *Gold: ${bank}*`,
+            footer: 'Levi 2023 🚀✨',
+            buttons: buttons,
+            headerType: 1
+        }
+        return void (await this.client.sendMessage(from, buttonMessage, {
+            quoted: message
+        }))
+    }
+}
